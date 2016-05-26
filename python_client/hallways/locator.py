e95f6949270e6e404958f03279182cfce4e50859
charmoniumQ <sam.grayson.ubuntu@gmail.com>
2016-05-26 10:11:29 -0500

    basic location finding (the real deal)

--- python_client/hallways/locator.py
+++ python_client/hallways/locator.py
@@ -1,8 +1,28 @@
-import numpy as np
+import numpy
+import scipy
 from .location import Location
 
-def locator(fingerprints, fields):
-    '''Estimates your location based on where the fields match your fingerprints'''
-    return Location(0, 0, 0)
+def locator(fingerprint, presence_fields, strength_fields):
+    '''Estimates your location based on where the fields match your fingerprint'''
+    o = scipy.optimize.fmin(cost, x0=Location(0, 0, 0), args=(presence_fields, strength_fields))
+    return o.x
+
+def presence_cost(location, fingerprint, presence_fields):
+    acc = 0
+    ctr = 0
+
+    return acc
+
+def strength_cost(location, fingerprint, strength_fields):
+    acc = 0
+    ctr = 0
+    for BSSID in fingerprint.networks.keys:
+        if BSSID in strength_fields:
+            acc += (strength_fields[BSSID](location) - fingerprint.networks[BSSID])**2
+            ctr += 1
+    return acc / ctr
+
+def cost(location, fingerprint, presence_fields, strength_fields):
+    return 1 * presence_cost + 1 * strenght_cost
 
 __all__ = ['locator']
